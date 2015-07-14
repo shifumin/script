aa519ecbf167d4d626ac1720d86506869bdd8f5f
shifumin <shifumin4230@gmail.com>
2015-07-14 17:56:06 +0900

    バックアップした旨のechoを追加

--- backup_kobito.sh
+++ backup_kobito.sh
@@ -12,23 +12,16 @@ if [ ! -e $backup_dir_path ]; then
 fi
 
 # バックアップファイルネームの作成
-date=(`date +"%Y%m%d"`)
-db_filename="Kobito.db.$date.bak"
-db_shm_filename="Kobito.db-shm.$date.bak"
-db_wal_filename="Kobito.db-wal.$date.bak"
+date=`date +"%Y%m%d"`
+db_filename=Kobito.db.$date.bak
+db_shm_filename=Kobito.db-shm.$date.bak
+db_wal_filename=Kobito.db-wal.$date.bak
 
 # バックアップ
-
 echo "バックアップディレクトリは $backup_dir_path です"
 
-if [ `cp -p $data_dir_path/Kobito.db $backup_dir_path/$db_filename` ]; then
-  echo "Kobito.db を $db_filename としてバックアップしました "
-fi
+cp -p $data_dir_path/Kobito.db $backup_dir_path/$db_filename && echo "Kobito.db を $db_filename としてバックアップしました"
 
-if [ `cp -p $data_dir_path/Kobito.db-shm $backup_dir_path/$db_shm_filename` ]; then
-  echo "Kobito.db-shm を $db_shm_filename としてバックアップしました "
-fi
+cp -p $data_dir_path/Kobito.db-shm $backup_dir_path/$db_shm_filename && echo "Kobito.db-shm を $db_shm_filename としてバックアップしました"
 
-if [ `cp -p $data_dir_path/Kobito.db-wal $backup_dir_path/$db_wal_filename` ]; then
-  echo "Kobito.db-wal を $db_wal_filename としてバックアップしました "
-fi
+cp -p $data_dir_path/Kobito.db-wal $backup_dir_path/$db_wal_filename && echo "Kobito.db-wal を $db_wal_filename としてバックアップしました"
